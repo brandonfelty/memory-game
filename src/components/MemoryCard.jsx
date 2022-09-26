bd6bed4da73c6e557712f7baed4e1969b78c57c2
Brandon Felty <brcfelty@gmail.com>
2022-09-26 14:38:51 -0600

    add back card

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import BackCard from './BackCard';
 import FrontCard from './FrontCard';
 
 export default function MemoryCard() {
   return (
     <section className='memory-card'>
       <FrontCard/>
+      <BackCard/>
     </section>
   )
 }
