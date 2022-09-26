14b60b4beaf0c25f1633d9ccaf6452f210ccd5f8
Brandon Felty <brcfelty@gmail.com>
2022-09-26 14:03:01 -0600

    change class name

--- src/components/BackCard.jsx
+++ src/components/BackCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function BackCard() {
   return (
-    <div className='back-card'>BackCard</div>
+    <div className='back-card'>
+      <img className='back-face' src={process.env.PUBLIC_URL + '/images/mist.png'} alt='Mist'></img>
+    </div>
   )
 }
