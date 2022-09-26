bd9d62c6e0272a79e64c39818d0330d257eccfb3
Brandon Felty <brcfelty@gmail.com>
2022-09-26 13:37:21 -0600

    add froont card image

--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Brain from '../images/brain.jpg'
 
 export default function FrontCard() {
   return (
-    <div className='front-card'>FrontCard</div>
+    <div className='front-card'>
+      <img className='front-face' src={Brain} alt='Brain'></img>
+    </div>
   )
 }
