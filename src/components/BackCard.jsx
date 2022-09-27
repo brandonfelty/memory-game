07de5ef614315d1a0291c0afde58ebf810330252
Brandon Felty <brcfelty@gmail.com>
2022-09-27 09:45:51 -0600

    change class names

--- src/components/BackCard.jsx
+++ src/components/BackCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function BackCard() {
   return (
-    <div className='back-card'>
-      <img className='back-face' src={process.env.PUBLIC_URL + '/images/brain.png'} alt='Brain'></img>
+    <div className='Back-Card'>
+      <img className='Back-Face' src={process.env.PUBLIC_URL + '/images/brain.png'} alt='Brain'></img>
     </div>
   )
 }
--- src/components/FrontCard.jsx
+++ src/components/FrontCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function FrontCard(props) {
   return (
-    <div className='front-card'>
-      <img className='front-face' src={process.env.PUBLIC_URL + '/images/' + props.frontImg + '.png'} alt='Front Card'></img>
+    <div className='Front-Card'>
+      <img className='Front-Face' src={process.env.PUBLIC_URL + '/images/' + props.frontImg + '.png'} alt='Front Card'></img>
     </div>
   )
 }
