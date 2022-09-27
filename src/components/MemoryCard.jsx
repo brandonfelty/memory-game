15e53f67ef2e6acb76bd2d0c96d1490192800917
Brandon Felty <brcfelty@gmail.com>
2022-09-27 14:42:19 -0600

    add flip styling and jsx

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BackCard from './BackCard';
 import FrontCard from './FrontCard';
 
 export default function MemoryCard(props) {
+  const [isVisible, setVisible] = useState(false);
+  const [isFlipping, setFlipping] = useState(false);
+
+  const handleClick = (e) => {
+    setFlipping(!isFlipping);
+  };
+
   return (
-    <div className='Memory-Card'>
-      <FrontCard frontImg={props.frontImg}/>
-      <BackCard/>
+    <div 
+      className=
+        {isFlipping 
+          ? 'Memory-Card Flip' 
+          : 'Memory-Card'
+        }
+      onClick={handleClick}>
+        {isVisible 
+          ? <FrontCard frontImg={props.frontImg}/> 
+          : <BackCard/>
+        }
     </div>
   )
 }
