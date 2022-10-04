31a4d6a3c9b86764a972e7617ea93a422ebe442a
Brandon Felty <brcfelty@gmail.com>
2022-10-04 14:06:44 -0600

    front and back cards loading

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import './styles/App.css';
 
 const cardImages = [
-  { "src": "/img/elk.png" },
-  { "src": "/img/flower.png" },
-  { "src": "/img/forest.png" },
-  { "src": "/img/mist.png" },
-  { "src": "/img/mountain.png" },
-  { "src": "/img/palm.png" }
+  { "src": "/images/elk Small.png" },
+  { "src": "/images/flower Small.png" },
+  { "src": "/images/forest Small.png" },
+  { "src": "/images/mist Small.png" },
+  { "src": "/images/mountain Small.png" },
+  { "src": "/images/palm Small.png" }
 ];
 
 function App() {
@@ -27,6 +27,24 @@ function App() {
     <div className="App">
       <h1>Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <div key={card.id} className="card">
+            <div>
+              <img 
+                src={process.env.PUBLIC_URL + card.src}
+                className="front" 
+                alt="card front">
+              </img>
+              <img 
+                src={process.env.PUBLIC_URL + '/images/brain Small.png'}
+                className="back"
+                alt="card back">
+              </img>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
