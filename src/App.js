0c8f3def03d7da35e38b77ef89d66e5f965b565c
Brandon Felty <brcfelty@gmail.com>
2022-10-04 13:21:40 -0600

    reset app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-import MemoryCard from './components/MemoryCard';
 import './styles/App.css';
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [disabled, setDisabled] = useState({});
 
   return (
-    <section className="Memory-Game">
-      <header className="App-header">
-      </header>
-      <MemoryCard disabled={disabled} setDisabled={setDisabled} frontImg="mist" cards={cards} setCards={setCards}/>
-      <MemoryCard disabled={disabled} setDisabled={setDisabled} frontImg="mist" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="forest" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="forest" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="palm" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="palm" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="sunrise" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="sunrise" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="flower" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="flower" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="elk" cards={cards} setCards={setCards}/>
-      <MemoryCard frontImg="elk" cards={cards} setCards={setCards}/>
-    </section>
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button>New Game</button>
+    </div>
   );
 }
 
