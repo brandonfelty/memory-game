032b4683999b83294513b120ba793fb5d72b5987
Brandon Felty <brcfelty@gmail.com>
2022-10-05 10:37:18 -0600

    add func to handle card choice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import SingleCard from './components/SingleCard';
 
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [firstCard, setFirstCard] = useState(null);
+  const [secondCard, setSecondCard] = useState(null);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -22,7 +24,32 @@ function App() {
 
     setCards(shuffledCards);
     setTurns(0);
-  }
+  };
+
+  const handleChoice = (card) => {
+    firstCard ? setSecondCard(card) : setFirstCard(card);
+  };
+
+  useEffect(() => {
+    checkChoice(firstCard, secondCard);
+  }, [secondCard]);
+
+  const checkChoice = (firstChoice, secondChoice) => {
+    if (firstChoice && secondChoice){
+      if (firstChoice.src === secondChoice.src){
+        console.log('Congrats they match!');
+      } else {
+        console.log('Sorry please try again');
+      }
+      resetTurn();
+    }
+  };
+
+  const resetTurn = () => {
+    setFirstCard(null);
+    setSecondCard(null);
+    setTurns(prevTurn => prevTurn++);
+  };
 
   return (
     <div className="App">
@@ -30,7 +57,7 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
         ))}
       </div>
     </div>
